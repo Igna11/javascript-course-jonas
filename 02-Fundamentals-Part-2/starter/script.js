596931fa377ee868ae418cc06aa464dbe6e6e05b
Ignacio Gomez <ignacio.gomez@avature.net>
2023-08-28 17:36:28 -0300

    asd

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,9 @@
 'use strict'; // strict mode to make more secure code. It is more verbose with errors
 
-let hasDriverLicence = false;
+let hasDriversLicence = false;
 const passTest = true;
 
+if (passTest) hasDriversLicence = true;
+if (hasDriversLicence) console.log("I can drive");
+
+const interface = "audio"; //-> this will throw this error: Uncaught SyntaxError: Unexpected strict mode reserved word (at script.js:9:7)

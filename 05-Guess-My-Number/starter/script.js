4e73aa52e0e1dd36366a22d6ee06acd72654d320
Igna <ignaao@gmail.com>
2023-09-14 00:08:01 -0300

    guess number
    copiando el juego. Los videos los sacaron de youtube asi que kbio

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+console.log(secretNumber);
+
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('⛔️No number!');
+  } else if (guess === secretNumber) {
+    displayMessage('You win dueeeeeee 🎉🎉');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').style.boxShadow =
+      'white 0px 0px 10px 5px';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+      console.log(score);
+    }
+  } else {
+    console.log(score);
+    displayMessage('💥You lost dude, try next time💥');
+    document.querySelector('body').style.backgroundColor = 'red';
+    document.querySelector('.score').textContent = 0;
+  }
+});

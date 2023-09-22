5c3df983ccafc63237f487d95a87e591ac1d9e97
Igna <ignaao@gmail.com>
2023-09-21 22:43:33 -0300

    clase 6

--- 06-Modal/final/script.js
+++ 06-Modal/final/script.js
@@ -4,7 +4,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-
+console.log('asdas');
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+console.log('aasdasdsd');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonCloseModal = document.querySelector('.close-modal');
+const buttonOpenModal = document.querySelectorAll('.show-modal');
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < buttonOpenModal.length; i++) {
+  buttonOpenModal[i].addEventListener('click', openModal);
+}
+
+buttonCloseModal.addEventListener('click', closeModal);
+//Cierra el modal al tocar en cualquier lado fuera del modal
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});

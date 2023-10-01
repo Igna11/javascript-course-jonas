0f18c9a9b8e0bc458791f4028f37e0e3adaee985
Igna <ignaao@gmail.com>
2023-09-30 21:50:31 -0300

    added the 'again' functionality to the game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,6 +9,17 @@ function displayMessage(message) {
   document.querySelector('.message').textContent = message;
 }
 
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').style.boxShadow = 'none';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+});
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 

9c9dabc6cbfd1cd7c92b47818a2aa108645678a5
Igna <ignaao@gmail.com>
2023-10-22 11:23:43 -0300

    asd

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/*
+
 function calcAge(birthYear) {
   const age = 2023 - birthYear;
   function printAge() {
@@ -16,3 +18,71 @@ function calcAge(birthYear) {
 
 const firsName = 'Igna';
 calcAge(1992);
+*/
+// THIS KEYWORD
+/*
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2023 - birthYear);
+  console.log(this); /// undefined
+};
+
+const calcAgeArrow = birthYear => {
+  console.log(2023 - birthYear);
+  console.log(this); /// undefined
+};
+
+calcAgeArrow(1991);
+
+const igna = {
+  year: 1992,
+  calcAge: function () {
+    console.log(this);
+  },
+};
+
+igna.calcAge();
+
+//this keyworks always points the objects that calls the method
+
+//98 Regular functions vs Arrow functions
+// arrow functions does not have they orwn "this" keyword.
+// var creates properties in the window object
+
+//99 Primitives vs objects
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge );
+*/
+
+//primitives types
+let lastName = 'gonzales';
+let oldlastname = lastName;
+lastName = 'garcía';
+console.log(lastName, oldlastname);
+
+// reference types
+const gonzalo = {
+  firstName: 'gonzalo',
+  lastName: 'garcía',
+  age: 30,
+};
+
+// copied types
+const gonzaloCasado = gonzalo; // NO crea un nuevo objeto en el heap, es una nueva variable en el stack que apunta a la misma dirección de memoria que gonzalo. Lo que le modifique a cualquiera (gonzalo o gonzaloCasado) es lo mismo para ambos
+gonzaloCasado.lastName = 'pedrelo';
+console.log('antes de casarse:', gonzalo);
+console.log('después de casarse:', gonzaloCasado);
+
+// solution to copy objects without modifying the original
+
+const gonzaloCasado2 = Object.assign({}, gonzalo); //shallow copy: Copy properties in the first level of the object
+gonzaloCasado2.lastName = 'pedrelin2';
+console.log(gonzaloCasado2, gonzalo);
+
+//por ejemplo, si hubiera un array en gonzalo y quisiera agregarle dos elementos al array de gonzaloCasado2, el array se modificaría también en gonzalo.
+// Para solucionar eso hay que hacer una deep copy, pero eso es dificil
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,7 +11,6 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
   openingHours: {
     thu: {
       open: 12,
@@ -22,8 +21,77 @@ const restaurant = {
       close: 23,
     },
     sat: {
-      open: 0, // Open 24 hours
+      open: 0,
       close: 24,
     },
   },
+  order: function (startedIndex, mainIndex) {
+    return [this.starterMenu[startedIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+//to de-strcuture objects we use curly braces {}
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+
+//mutating variables
+let a = 111;
+let b = 999;
+
+const obj = { a: 0, b: 10, c: 100 };
+({ a, b } = obj);
+console.log(a, b);
+
+const {
+  fri: { open: o = 1, close: c = 1, mid: m = 1 },
+} = openingHours;
+
+console.log(o, c, m);
+/*
+const arr = [2, 3, 4];
+
+const [x, y, z] = arr;
+
+console.log(x, y, z);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+*/
+/* switching variables: first solution
+const temp = main;
+main = secondary;
+secondary = temp;
+
+console.log(secondary, main);
+*/
+
+/* switching variables: second solution
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+console.log(restaurant.order(1, 1));
+
+//we can de-structure it
+const [starter, mainCourse] = restaurant.order(2, 1);
+console.log(starter, mainCourse);
+
+const nested = [2, 3, [4, 5]];
+*/
+/*
+const [i, , j] = nested;
+console.log(i, j);*/
+
+/*
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+*/
